3309790db482e683cbc402d17d19e7c71f41ba83
mihail <76650506+mihail666@users.noreply.github.com>
2022-03-20 20:54:58 +0500

    Add files via upload
    бутстрап слетел и все поехало написал новый сайт+анимация и адаптация

--- website1/srf/js/script.js
+++ website1/srf/js/script.js
@@ -19,3 +19,7 @@ window.addEventListener("scroll", function (){
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 })
+function toggle(){
+    var header =document.querySelector("header");
+    header.classList.toggle("active");
+}
